3d007d38d238fa6c8cfe3258a003127851b4660f
김민지 <133857306+minzlzl@users.noreply.github.com>
2023-10-29 12:38:02 +0900

    231029

--- js/typing.js
+++ js/typing.js
@@ -1,13 +1,30 @@
-const imGround = "안녕하세요? \n 웹 퍼블리셔 김민지의 포트폴리오 사이트에 \n 방문해 주셔서 감사합니다 ! !";
-const text = document.querySelector(".text");
-let i = 0;
+// const imGround = "안녕하세요? \n 웹 퍼블리셔 김민지의 포트폴리오 사이트에 \n 방문해 주셔서 감사합니다 ! !";
+// const text = document.querySelector(".text");
+// let i = 0;
 
-function typing(){
-let txt = imGround[i++];
-text.innerHTML += txt=== "\n" ? "<br/>": txt;
-if(i > imGround.length){
-    text.textContent = "";
-    i = 0;
+// function typing(){
+// let txt = imGround[i++];
+// text.innerHTML += txt=== "\n" ? "<br/>": txt;
+// if(i > imGround.length){
+//     text.textContent = "";
+//     i = 0;
+//   }
+// }
+// setInterval(typing, 100)
+
+
+window.onload = function(){
+  const imGround = "안녕하세요? \n 웹 퍼블리셔 김민지의 포트폴리오 사이트에 \n 방문해 주셔서 감사합니다 ! !";
+  const text = document.querySelector(".text");
+  let i = 0;
+
+  function typing() {
+    let txt = imGround[i++];
+    text.innerHTML += txt === "\n" ? "<br/>" : txt;
+    if (i < imGround.length) {
+      setTimeout(typing, 100); // 다음 타이핑을 예약
+    }
   }
-}
-setInterval(typing, 100)
+
+  typing(); // 최초 한 번 실행
+};
