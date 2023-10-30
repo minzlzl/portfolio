9cef124af23bad1992edff4453e4b077907b33ca
김민지 <133857306+minzlzl@users.noreply.github.com>
2023-10-30 16:47:32 +0900

    231030

--- js/header.js
+++ js/header.js
@@ -14,23 +14,38 @@ const home = document.querySelector('.header_wrap>ul>li:nth-of-type(1)>a'),
           about = document.querySelector('.header_wrap>ul>li:nth-of-type(2)>a'),
           project = document.querySelector('.header_wrap>ul>li:nth-of-type(3)>a'),
           design = document.querySelector('.header_wrap>ul>li:nth-of-type(4)>a'),
+          conWrap = document.querySelectorAll('main > div'),
           con1Wrap = document.querySelector('.con1_wrap'),
           con2Wrap = document.querySelector('.con2_wrap'),
           con3Wrap = document.querySelector('.con3_wrap'),
           con4Wrap = document.querySelector('.con4_wrap')
 home.addEventListener("click",()=>{
   window.scrollBy({top: con1Wrap.getBoundingClientRect().top, behavior: 'smooth'});
-  header.style = `background:#fafafa`
 });
 about.addEventListener("click",function(){
   window.scrollBy({top: con2Wrap.getBoundingClientRect().top, behavior: 'smooth'});
-  header.style = `background:#fff`
 });
 project.addEventListener("click",function(){
   window.scrollBy({top: con3Wrap.getBoundingClientRect().top, behavior: 'smooth'});
-  header.style = `background:#fafafa`
 });
 design.addEventListener("click",function(){
   window.scrollBy({top: con4Wrap.getBoundingClientRect().top, behavior: 'smooth'});
-  header.style = `background:#fff`
 });
+
+let nums = 0;
+window.addEventListener('scroll',()=>{
+  conWrap.forEach((v,k)=>{
+    if( conWrap[k].getBoundingClientRect().top <= 100 ){
+      ela.forEach(v2=>{
+        ela[nums].classList.remove('colors');
+        ela[k].classList.add('colors');
+        nums=k;
+        if(k == 1 || k == 3){
+          header.style = `background:#fff`
+        }else{
+          header.style = `background:#fafafa`
+        }
+      })
+    }
+  })
+})
