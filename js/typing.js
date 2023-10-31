1dce50072851aeaa3dfa2ed059f7df8c8c7a3a1c
김민지 <133857306+minzlzl@users.noreply.github.com>
2023-10-31 17:17:03 +0900

    231031

--- js/typing.js
+++ js/typing.js
@@ -14,7 +14,7 @@
 
 
 window.onload = function(){
-  const imGround = "안녕하세요? \n 웹 퍼블리셔 김민지의 포트폴리오 사이트에 \n 방문해 주셔서 감사합니다 ! !";
+  const imGround = "let experience = (design)=>{return code;}";
   const text = document.querySelector(".text");
   let i = 0;
 
