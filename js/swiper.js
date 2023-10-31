2bead18dcfd5981cc1d9e08e0432538ddcb1ae8b
김민지 <133857306+minzlzl@users.noreply.github.com>
2023-10-31 11:12:53 +0900

    231031

--- js/swiper.js
+++ js/swiper.js
@@ -11,3 +11,15 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+var swiper = new Swiper(".mySwipers", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
