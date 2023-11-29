f9d44dea413ab9c99f8a36f1cd373623561b80f2
김민지 <133857306+minzlzl@users.noreply.github.com>
2023-11-29 11:38:03 +0900

    231129

--- js/popup.js
+++ js/popup.js
@@ -18,8 +18,6 @@ const reviewClick = document.querySelectorAll('.project_btn button:nth-of-type(2
       reviewBg = document.querySelector('.review_bg'),
       ts = document.querySelectorAll('.ts');
 
-
-
 reviewClick.forEach(function(v2,k2){
   let count = 0;
   let num = 0;
@@ -29,31 +27,20 @@ reviewClick.forEach(function(v2,k2){
     count = k2;
     ts.forEach((v3,k3)=>{
       if(count == 0){
-        if(k3 == 0){
-          ts[num].style = `display:none`
-          v3.style = `display:block`
-        }
+        ts[num].style = `display:none`;
+        ts[0].style = `display:block`;
       }else if(count == 1){
-        if(k3 == 1){
-          ts[num].style = `display:none`
-          v3.style = `display:block`
-        }
+        ts[num].style = `display:none`;
+        ts[1].style = `display:block`;
+      }else if(count == 2){
+        ts[num].style = `display:none`;
+        ts[2].style = `display:block`;
+      }else if(count == 3){
+        ts[num].style = `display:none`;
+        ts[3].style = `display:block`;
       }
-      else if(count == 2){
-        if(k3 == 2){
-          ts[num].style = `display:none`
-          v3.style = `display:block`
-        }
-      }
-      else if(count == 3){
-        if(k3 == 3){
-          ts[num].style = `display:none`
-          v3.style = `display:block`
-        }
-      }
-      num = k3
+      num = k3;
     })
-
     reviewBg.onclick = function(){
       reviewBg.style = `display:none`
       body.style = `overflow:auto`
